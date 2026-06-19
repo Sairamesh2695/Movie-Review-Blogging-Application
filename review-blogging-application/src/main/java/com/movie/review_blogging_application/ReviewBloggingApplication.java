ea9428eddd064f7e70b48943f2619342164c3086
RameshP26 <sairameshkumarp@gmail.com>
2026-06-19 22:23:25 +0530

    First Commit

---
+++ review-blogging-application/src/main/java/com/movie/review_blogging_application/ReviewBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.movie.review_blogging_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewBloggingApplication.class, args);
+	}
+
+}
---
+++ review-blogging-application/src/test/java/com/movie/review_blogging_application/ReviewBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.review_blogging_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
